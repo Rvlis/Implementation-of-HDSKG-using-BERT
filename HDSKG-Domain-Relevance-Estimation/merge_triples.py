ed4052e6135ecae441bbc2e5e12422730ae85927
Rvlis <1065013799@qq.com>
2021-04-20 09:23:21 +0800

    upload source code

--- HDSKG-Domain-Relevance-Estimation/merge_triples.py
+++ HDSKG-Domain-Relevance-Estimation/merge_triples.py
@@ -9,41 +9,34 @@ import pandas as pd
 import os
 from scripts import myutils
 import csv
+from tqdm import tqdm
 
 # process train set
-src_csv_path = open("./csvs/train_set - 副本.csv")
+src_csv_path = open("../csvs/manual_labeled_train_set.csv")
 tar_csv = myutils.open_csv("processed_train_set")
 
 tar_csv.writerow(["entity1", "relation", "entity2", "merged_sentence", "label"])
 
 src_csv = csv.reader(src_csv_path)
 
-for line in src_csv:
+print("..........process manual labelled train set..........")
+for line in tqdm(src_csv):
     merged_sentence = line[0].lower().strip() + " " + line[1].lower().strip() + " " + line[2].lower().strip()
     label = line[3]
-    # if line[3] == "T":
-    #     label = 1
-    # else:
-    #     label = 0
-    # print(candidate_relation_triple, label)
     tar_csv.writerow([line[0], line[1], line[2], merged_sentence, label])
 
 
 # process test set
-src_csv_path = open("./csvs/test_set - 副本.csv")
+src_csv_path = open("../csvs/manual_labeled_test_set.csv")
 tar_csv = myutils.open_csv("processed_test_set")
 
 tar_csv.writerow(["entity1", "relation", "entity2", "merged_sentence", "label"])
 
 src_csv = csv.reader(src_csv_path)
 
-for line in src_csv:
+print("..........process manual labelled test set..........")
+for line in tqdm(src_csv):
     merged_sentence = line[0].lower().strip() + " " + line[1].lower().strip() + " " + line[2].lower().strip()
     label = line[3]
-    # if line[3] == "T":
-    #     label = 1
-    # else:
-    #     label = 0
-    # print(candidate_relation_triple, label)
     tar_csv.writerow([line[0], line[1], line[2], merged_sentence, label])
 
--- HDSKG-Domain-Relevance-Estimation/process_train_test_set.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-train_set_csv_path = "./csvs/processed_train_set.csv"
-test_set_csv_path = "./csvs/processed_test_set.csv"
-
-train_set = pd.read_csv(train_set_csv_path)
-test_set = pd.read_csv(test_set_csv_path)
